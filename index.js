348924a99eecf66dee68fe905477ab477760a577
sohan <er.skchoudhary22@gmail.com>
2024-07-12 10:48:22 +0530

    u

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 let name = document.getElementById("name");
 let email = document.getElementById("email");
+let course = document.getElementById("course");
+let mobile = document.getElementById("mobile");
 let photo = document.getElementById("photo");
 let btn = document.getElementById("btn");
 let cont = document.getElementById("cont");
+let form= document.getElementById("form")
 let state = false;
 let currentid = null;
 let arr = [];
@@ -10,12 +13,17 @@ async function postData(){
     let obj = {
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
+        course: document.getElementById("course").value,
+        mobile: document.getElementById("mobile").value,
         photo: document.getElementById("photo").value,
     };
 
     try{
         let res = await axios.post("http://localhost:3000/user",obj);
         alert("submitted");
+        getData()
+        form.reset();
+        // console.log(res);
     } catch (error){
         console.log(error);
     }
@@ -26,61 +34,77 @@ async function getData(){
         let res = await axios.get("http://localhost:3000/user");
         console.log(res.data)
         arr = res.data;
-        displayData(arr);
+        displayData();
     } catch (error){
         console.log(error);
     }
 }
 
-getData();
 
-// <============delete=====================>
-    async function deleteData(id){
-        try{
-            let res = await axios.delete(`http://localhost:3000/user/${id}`)
-            getData();
-        } catch (error){
-            console.log(error);
-        }
-    }
 
-function displayData(data) {
+
+function displayData() {
     cont.innerHtml= ""
-    data.map((ele) =>{
+    arr.map((ele) =>{
         
         let div = document.createElement("div");
         let name= document.createElement("name");
         name.innerText = ele.name;
         let email = document.createElement("email");
         email.innerText = ele.email;
+        let course = document.createElement("course");
+         course.innerText = ele.course;
+        let mobile = document.createElement("mobile");
+        mobile.innerText = ele.mobile;
         let photo = document.createElement("img");
         photo.src = ele.photo;
+        let div1=document.createElement("div1")
         let btn = document.createElement("button");
         btn.innerText = "edit";
+
+        btn.style.padding="5px 25px"
+        btn.style.backgroundColor="limegreen"
+        btn.style.color="white"
         btn.addEventListener("click", () =>{
             getDataforEdit(ele);
         });
         let del = document.createElement("button");
         del.innerText = "delete";
 
+        del.style.backgroundColor="firebrick"
+        del.style.color="white"
         del.addEventListener("click",() =>{
              deleteData(ele.id);
-             alert("deleted");
+            
         });
-
-        div.append(photo,name,email,btn,del);
+       div1.append(btn,del)
+        div.append(photo,name,email,course,mobile,div1);
         cont.append(div);
     });
    
 }
-displayData(arr);
+
+// <============delete=====================>
+    async function deleteData(id){
+        try{
+            let res = await axios.delete(`http://localhost:3000/user/${id}`)
+            alert("deleted");
+            getData();
+                } catch (error){
+            console.log(error);
+        }
+    }
+
+// displayData(arr);
 
 // <================edit==================>
     function getDataforEdit(data) {
         let obj = {
             name: document.getElementById("name").value = data.name,
             email: document.getElementById("email").value = data.email,
-            photo: document.getElementById("photo").value = data.photo,
+            course: document.getElementById("course").value = data.course,
+            mobile: document.getElementById("mobile").value = data.mobile,
+            photo: document.getElementById("photo").value = data.photo
         };
         btn.innerText = "update";
         state = true;
@@ -94,20 +118,26 @@ displayData(arr);
         let updateobj = {
             name: document.getElementById("name").value,
             email: document.getElementById("email").value,
+            course: document.getElementById("course").value,
+            mobile: document.getElementById("mobile").value,
             photo: document.getElementById("photo").value,
         };
         try{
             let res = await axios.patch(`http://localhost:3000/user/${currentid}`,updateobj);
             alert("updated");
+            getData()
             state = false;
+            form.reset()
         } catch (error) {
             console.log(error);
         }
     }
-    btn.addEventListener("click", () =>{
+    btn.addEventListener("click", (e) =>{
+        e.preventDefault()
         if (state===true){
             updatedata();
         } else {
             postData();
         }
     });        
+    getData();
